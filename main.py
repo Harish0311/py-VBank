a59068fd827f9fcccea4ab6b16f51d7e8e5943ee
Harish <harishgamer00@gmail.com>
2022-09-24 11:25:00 +0530

    login_infinteloop_fix

--- main.py
+++ main.py
@@ -13,44 +13,54 @@ while n != 3 :
     print("What do u wanna do ? : ")
     if not(isLoggedIn):
         print("1). Create account     2). Login      3). Quit")
+        n = int(input("Enter Here : "))
+    
+        if n == 1 :
+            name = input("Name     : ")
+            no   = input("Number   : ")
+            pwd  = input("Password : ")
+            data ={
+                "Name" : name,
+                "Number" : no,
+                "Password" : pwd
+            }
+            length = len(db)
+            flag = 0
+            for i in range(length):
+                if(db[i]["Number"]==no):
+                    flag = 1
+                    break
+                
+            if flag==1 :
+                print("Account already exists ! ")
+                
+            else :
+                db.append(data)
+                print("Account created Successfully !")
+            reset = input("Press Enter to continue")
+            helpers.clearConsole()
+        elif n == 2 :
+            no   = input("Number   : ")
+            pwd  = input("Password : ")
+            length = len(db)
+            for i in range(length):
+                if(db[i]["Number"]==no):
+                    if(db[i]["Password"]==pwd):
+                        helpers.clearConsole()
+                        isLoggedIn = True
+                    else :
+                        print("Wrong password")
+                        rprint("Account created Successfully !")
+                        reset = input("Press Enter to continue")
+                        helpers.clearConsole()
+
     else:
+    
         print("1). Transfer money     2). Logout      3). Quit")
-        
-    n = int(input("Enter Here : "))
-   
-    if n == 1 :
-        name = input("Name     : ")
-        no   = input("Number   : ")
-        pwd  = input("Password : ")
-        data ={
-            "Name" : name,
-            "Number" : no,
-            "Password" : pwd
-        }
-        length = len(db)
-        flag = 0
-        for i in range(length):
-            if(db[i]["Number"]==no):
-                flag = 1
-                break
-            
-        if flag==1 :
-            print("Account already exists ! ")
-            
-        else :
-            db.append(data)
-            print("Account created Successfully !")
-        reet = input("Press Enter to continue")
-        helpers.clearConsole()
-    elif n == 2 :
-        no   = input("Number   : ")
-        pwd  = input("Password : ")
-        length = len(db)
-        for i in range(length):
-            if(db[i]["Number"]==no):
-                if(db[i]["Password"]==pwd):
-                    helpers.clearConsole()
-                    isLoggedIn = True
+        n = int(input("Enter Here : "))
+    
+                   
+
 
     
 
